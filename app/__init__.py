b14bbc517d798095df9674a1b0cf650a2b9f6449
hayaboosan <hayaboosan@gmail.com>
2022-07-08 08:33:43 +0900

    [Add]サーバー起動する環境構築

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+import os
+
+
+from config import DATABASE_URI
+from config import engine
+
+
+db = SQLAlchemy()
+session = scoped_session(sessionmaker(
+    autocommit=False, autoflush=False, bind=engine))
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
